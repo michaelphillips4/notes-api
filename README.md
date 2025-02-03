003800cfda13972502a806da3244c56ff9d87ba2
michaelp <michaelp@linux-laptop.lan>
2025-02-03 17:57:58 +0000

    various

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Elysia with Bun runtime
 
 ## Getting Started
-To get started with this template, simply paste this command into your terminal:
+
 ```bash
-bun create elysia ./elysia-example
+curl -fsSL https://bun.sh/install | bash
+bun create elysia notes-api //already done
+bun install
+bun run
 ```
 
 ## Development
@@ -13,3 +16,6 @@ bun run dev
 ```
 
 Open http://localhost:3000/ with your browser to see the result.
+
+
+https://elysiajs.com/
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,32 @@
-import { Elysia } from "elysia";
+import { Elysia,t } from "elysia";
+import { swagger } from '@elysiajs/swagger';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+
+class Note { 
+  constructor(public data: string[] = ['Moonhalo']) {} 
+} 
+
+
+const app = new Elysia()
+// Apply the swagger plugin
+  .use(swagger()) 
+  .get("/", () => "Hello Elysia")
+  .get("/hello", () => "Hello Elysia")
+  .get('/echo', ({ path }) => path) 
+  .post("/hello", "Do you miss me?")
+  .decorate('note', new Note()) 
+  .get('/note', ({ note }) => note.data) 
+  .get(
+    '/note/:index',
+    ({ note, params: { index } }) => {
+        return note.data[index]
+    },
+    { 
+        params: t.Object({ 
+            index: t.Number() 
+        }) 
+    } )
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
